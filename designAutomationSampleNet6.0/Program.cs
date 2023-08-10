c5fb97c752b6090aae213938d2426712b97117c2
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-08-09 17:10:02 -0700

    created server

---
+++ designAutomationSampleNet6.0/Program.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Forge.Core;
+using Autodesk.Forge.DesignAutomation;
+
+
+namespace designAutomationSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).ConfigureAppConfiguration(builder =>
+            {
+                builder.AddJsonFile($"appsettings.user.json", optional: true);
+                builder.AddEnvironmentVariables();
+
+            }).ConfigureServices((hostContext, services) =>
+            {
+                services.AddDesignAutomation(hostContext.Configuration);
+            }).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
+
